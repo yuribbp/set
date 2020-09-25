5991296fb6a0515a6df6120e4b6c0657d5dbc441
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 15:26:24 -0300

    Treeset test

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Set<String> set = new HashSet<>();
+		Set<String> set = new TreeSet<>();
 		
 		set.add("TV");
 		set.add("Tablet");
