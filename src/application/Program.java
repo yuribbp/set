44277a09d35c746c3859fcaec3c21ca5cf45fbb7
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 15:33:19 -0300

    set.removeIf(x -> x.charAt(0) == 'T')

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 		set.add("Tablet");
 		set.add("Notebook");
 		
-		set.removeIf(x -> x.length()>= 3);
+		set.removeIf(x -> x.charAt(0) == 'T');
 		
 		for (String p : set) {
 			System.out.println(p);
