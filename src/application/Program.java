9e913456c388827f2e1a0d553ebedb178e9520b4
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 15:31:55 -0300

    set.removeIf(x -> x.lenght() >= 3)

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 		set.add("Tablet");
 		set.add("Notebook");
 		
-		set.remove("Tablet");
+		set.removeIf(x -> x.length()>= 3);
 		
 		for (String p : set) {
 			System.out.println(p);
