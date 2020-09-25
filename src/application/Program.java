1c5c18e993ab449feec8fecf4a7ae69f120a0872
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 15:35:42 -0300

    Demo 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,16 +8,22 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Set<String> set = new LinkedHashSet<>();
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 		
-		set.add("Tv");
-		set.add("Tablet");
-		set.add("Notebook");
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 		
-		set.removeIf(x -> x.charAt(0) == 'T');
+		//intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
 		
-		for (String p : set) {
-			System.out.println(p);
-		}
+		//difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 }
